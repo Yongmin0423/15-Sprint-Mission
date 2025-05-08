7848ba56130dd22c81e549e2223f12d45862e837
Yongmin0423 <yym21345@gmail.com>
2025-05-08 15:52:31 +0900

    feat: router 설정 및 nav 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Nav from "./components/Nav";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Nav />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
