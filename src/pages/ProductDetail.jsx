1cbc6335bde076ac278992a9f94bd252618854cd
Yongmin0423 <yym21345@gmail.com>
2025-05-10 18:16:25 +0900

    feat: detail 페이지 상품 소개 및 문의하기

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,121 @@
+import { useParams } from "react-router-dom";
+import { getItemDetail } from "../apis/detailApi";
+import { useEffect, useState } from "react";
+import noImage from "../assets/images/no-image.png";
+import avatar from "../assets/images/avatar.png";
+import HeartIcon from "../assets/icons/icon_heart";
+import VerticalEllipsis from "../assets/icons/icon_vertical-ellipsis";
+
+export default function ProductDetail() {
+  const [item, setItem] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { productId } = useParams();
+
+  const getDetailData = async () => {
+    try {
+      setIsLoading(true);
+      const data = await getItemDetail(productId);
+      setItem(data);
+    } catch (error) {
+      console.error("상품 상세 데이터 오류", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (productId) {
+      getDetailData();
+    }
+  }, [productId]);
+
+  console.log(item);
+
+  if (isLoading) {
+    return <p className="text-center animate-bounce ">로딩 중..</p>;
+  }
+
+  if (!item) {
+    return <p className="text-center animate-bounce">상품 정보가 없습니다.</p>;
+  }
+
+  return (
+    <div className="max-w-[120rem] mx-auto p-7">
+      <div className="flex w-full h-[25%] gap-10 mx-auto text-[#4B5563] border-b-gray-200 border-b pb-10">
+        <div className="w-[35%] h-full">
+          <img
+            className="h-full aspect-1/1 rounded-3xl"
+            src={item.images[0] || noImage}
+          />
+        </div>
+        <div className="flex flex-col w-[65%]">
+          <div className="flex flex-col gap-5 mb-5 text-[#1F2937]">
+            <div className="flex justify-between">
+              <h3 className="text-[2rem] font-[600]">{item.name}</h3>
+              <VerticalEllipsis />
+            </div>
+            <h1 className="text-[3.2rem] font-[600]">
+              {item.price.toLocaleString()}원
+            </h1>
+          </div>
+          <div className="flex flex-col gap-5 mb-5">
+            <h5 className="text-[1.4rem] font-[600]">상품 소개</h5>
+            <p className="text-[1.2rem] font-[400] h-[10vh]">
+              {item.description}
+            </p>
+          </div>
+          <div className="flex flex-col gap-5 mb-5">
+            <h5 className="text-[1.4rem] font-[600]">상품 태그</h5>
+            <div className="flex gap-2 flex-wrap mt-2">
+              {item.tags &&
+                item.tags.map((tag) => (
+                  <div
+                    key={tag}
+                    className="bg-gray-100 text-black px-3 py-2 rounded-full flex items-center text-[1.2rem]"
+                  >
+                    #{tag}
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="flex w-full justify-between items-center bg-transparent">
+            <div className="flex h-full items-center gap-3">
+              <img
+                src={avatar}
+                className="bg-gray-300 rounded-full size-[4rem]"
+                alt="Owner Avatar"
+              />
+              <div className="flex flex-col py-1 justify-between h-full">
+                <p>{item.ownerNickname}</p>
+                <p>{item.updatedAt}</p>
+              </div>
+            </div>
+            <div className="flex border border-[#E5E7EB] rounded-full px-2 py-0.5">
+              <HeartIcon />
+              <p>{item.favoriteCount}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h3>문의하기</h3>
+        <textarea
+          id="productDescription"
+          placeholder="개인정보를 공유 및 요청하시거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
+          rows="5"
+          className="w-full p-4 bg-[#F3F4F6] placeholder:text-[#9CA3AF] placeholder:text-[1.4rem] "
+        />
+        <div className="flex justify-end ">
+          <button
+            type="submit"
+            className="py-2.5 px-4 rounded-md transition-colors text-white cursor-pointer
+        bg-gray-400 disabled:cursor-not-allowed"
+          >
+            등록
+          </button>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
